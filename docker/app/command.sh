e9a08b3e15ba6cf80679c12c8461ac3530c75dc0
Thai Truong <truongvanthai12@gmail.com>
2024-06-19 23:47:24 +0700

    fix docker compose

--- docker/app/command.sh
+++ docker/app/command.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
 # Remove a potentially pre-existing server.pid for Rails.
-rm -f /app/tmp/pids/server.pid
-
-bundle check || bundle install
-
 bundle exec rails db:prepare && bundle exec rails s -p 3000 -b 0.0.0.0
